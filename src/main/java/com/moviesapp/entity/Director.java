d697f60b9a6dd39c586a0e752922dd9a8500b361
Javier Cota <javicota10@gmail.com>
2022-11-30 16:12:41 -0700

    Adds Director and Studio entities.

--- src/main/java/com/moviesapp/entity/Director.java
+++ src/main/java/com/moviesapp/entity/Director.java
@@ -1,4 +1,82 @@
 package com.moviesapp.entity;
 
+import java.util.Date;
+import java.util.Objects;
+
 public class Director {
+    private String directorName;
+    private Date birthDate;
+    private String nationality;
+    private String activeYears;
+    private String education;
+
+    public Director(String directorName, Date birthDate, String nationality, String activeYears, String education) {
+        this.directorName = directorName;
+        this.birthDate = birthDate;
+        this.nationality = nationality;
+        this.activeYears = activeYears;
+        this.education = education;
+    }
+
+    public String getDirectorName() {
+        return directorName;
+    }
+
+    public void setDirectorName(String directorName) {
+        this.directorName = directorName;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getActiveYears() {
+        return activeYears;
+    }
+
+    public void setActiveYears(String activeYears) {
+        this.activeYears = activeYears;
+    }
+
+    public String getEducation() {
+        return education;
+    }
+
+    public void setEducation(String education) {
+        this.education = education;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Director)) return false;
+        Director director = (Director) o;
+        return directorName.equals(director.directorName) && birthDate.equals(director.birthDate) && Objects.equals(nationality, director.nationality) && Objects.equals(activeYears, director.activeYears) && Objects.equals(education, director.education);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(directorName, birthDate, nationality, activeYears, education);
+    }
+    @Override
+    public String toString() {
+        return "Director{" +
+                "directorName='" + directorName + '\'' +
+                ", birthDate=" + birthDate +
+                ", nationality='" + nationality + '\'' +
+                ", activeYears='" + activeYears + '\'' +
+                ", education='" + education + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/moviesapp/entity/Studio.java
+++ src/main/java/com/moviesapp/entity/Studio.java
@@ -1,4 +1,73 @@
 package com.moviesapp.entity;
 
+import java.util.Date;
+import java.util.Objects;
+
 public class Studio {
+    private String studioName;
+    private Date foundation;
+    private String founder;
+    private String headquarters;
+
+    public Studio(String studioName, Date foundation, String founder, String headquarters) {
+        this.studioName = studioName;
+        this.foundation = foundation;
+        this.founder = founder;
+        this.headquarters = headquarters;
+    }
+
+    public String getStudioName() {
+        return studioName;
+    }
+
+    public void setStudioName(String studioName) {
+        this.studioName = studioName;
+    }
+
+    public Date getFoundation() {
+        return foundation;
+    }
+
+    public void setFoundation(Date foundation) {
+        this.foundation = foundation;
+    }
+
+    public String getFounder() {
+        return founder;
+    }
+
+    public void setFounder(String founder) {
+        this.founder = founder;
+    }
+
+    public String getHeadquarters() {
+        return headquarters;
+    }
+
+    public void setHeadquarters(String headquarters) {
+        this.headquarters = headquarters;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Studio)) return false;
+        Studio studio = (Studio) o;
+        return studioName.equals(studio.studioName) && foundation.equals(studio.foundation) && Objects.equals(founder, studio.founder) && Objects.equals(headquarters, studio.headquarters);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(studioName, foundation, founder, headquarters);
+    }
+
+    @Override
+    public String toString() {
+        return "Studio{" +
+                "studioName='" + studioName + '\'' +
+                ", foundation=" + foundation +
+                ", founder='" + founder + '\'' +
+                ", headquarters='" + headquarters + '\'' +
+                '}';
+    }
 }
