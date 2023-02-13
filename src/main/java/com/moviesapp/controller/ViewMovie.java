e415458a43930730bc162ceb31135b15a2f59cb5
Javier Cota <javicota10@gmail.com>
2023-02-13 12:13:34 -0700

    Adds viewMovie form and creates viewMPanel.

---
+++ src/main/java/com/moviesapp/controller/ViewMovie.java
@@ -0,0 +1,19 @@
+package com.moviesapp.controller;
+
+import javax.swing.*;
+
+public class ViewMovie {
+    private JTextField viewMName;
+    private JPanel panel1;
+    private JTextField viewMGenre;
+    private JTextField viewMDuration;
+    private JTextField viewMClassification;
+    private JTextField viewMReleaseD;
+    private JTextField viewMDescription;
+    private JLabel viewMDirID;
+    private JLabel viewMStudioID;
+    private JPanel viewMPanel;
+    private JButton viewMReturnBtn;
+    private JButton viewMUpdateBtn;
+    private JButton viewMDeleteBtn;
+}
